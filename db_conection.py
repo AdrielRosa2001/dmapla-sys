4692abb27eef54c1dfd7624aedad68c5a51f889b
Adriel Rosa <minilord002@gmail.com>
2023-08-03 18:46:12 -0300

    add gif de update da nova funcionalidade e pequena modificação para implementação futura no modulo db_conection.py

--- db_conection.py
+++ db_conection.py
@@ -44,6 +44,24 @@ class ServerDB():
                 return dicionario
             else:
                 return None
+    
+    def consulta_tuple_quant(self, codigo):
+        if self.sessao == True:
+            self.cursor.execute(f"SELECT [produto], [precovenda] FROM [SICNET_139726].[dbo].[TABEST1] WHERE [codigo] = '{codigo}'")
+            row = self.cursor.fetchone()
+            if row:
+                codigo_item = codigo
+                titulo = row[0]
+                valor = str(round(row[1], 2)).replace(".", ",")
+                dicionario = {
+                    "titulo": titulo,
+                    "codigo": codigo_item,
+                    "preco": f"R$ {valor}"
+                }
+                #return dict({"PRODUTO": row[0], "VALOR": str(round(row[1], 2)).replace(".", ",") })
+                return dicionario
+            else:
+                return None
             
 
 
